88c05892c71ca18543a10fa17236be82dd8418e6
Maykel <shalumov97@gmail.com>
2026-09-09 14:17:58 +0300

    Project Localization Add

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,30 @@
+import Hero from '@/components/common/Hero';
+import Header from '@/components/UI/Header';
+import { getTranslations } from 'next-intl/server';
+
+type TPageProps = {
+	params: Promise<{
+		locale: string;
+	}>;
+};
+
+export default async function Page({ params }: TPageProps) {
+	const { locale } = await params;
+	const t = await getTranslations('home.hero');
+
+	const hero = {
+		eyebrow: t('eyebrow'),
+		title: t('title'),
+		subtitle: t('subtitle'),
+		descr: t('description'),
+		buttonText: t('button'),
+		note: t('note'),
+	};
+
+	return (
+		<>
+			<Header locale={locale} />
+			<Hero hero={hero} />
+		</>
+	);
+}
